7c76b1bdfad8016eb2a421a8ede7d6f4e3cc0f49
gnuh24 <hungnt.020404@gmail.com>
2025-10-06 01:20:03 +0700

    Project1: Setup Module Maven cho project

---
+++ sa_project1/sa_project1_backend/order-service/order-container/src/main/java/sgu/sa/order/OrderServiceApplication.java
@@ -0,0 +1,12 @@
+package sgu.sa.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class OrderServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+}
