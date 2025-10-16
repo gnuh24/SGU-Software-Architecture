f52237b8abb6c51aa1f84caa018502131922dd44
hidenseek9415 <the.first9415@gmail.com>
2025-10-16 13:55:30 +0700

    WIP(order): đơn giản hóa quy trình xử lý event

--- project/order-service/order-messaging/src/main/java/sgu/sa/messaging/consumer/PaymentCompletedKafkaConsumer.java
+++ project/order-service/order-messaging/src/main/java/sgu/sa/messaging/consumer/PaymentCompletedKafkaConsumer.java
@@ -5,15 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import sgu.sa.application.event.paymentcompleted.PaymentCompletedHandler;
-import sgu.sa.messaging.mapper.PaymentEventMapper;
+import sgu.sa.application.usecase.command.payorder.PayOrderCommand;
+import sgu.sa.application.usecase.command.payorder.PayOrderHandler;
 
 @Component
 @RequiredArgsConstructor
 public class PaymentCompletedKafkaConsumer {
 
-    private final PaymentCompletedHandler handler;
-    private final PaymentEventMapper mapper;
+    private final PayOrderHandler payOrderHandler;
 
     @Transactional
     @KafkaListener(
@@ -22,6 +21,7 @@ public class PaymentCompletedKafkaConsumer {
         containerFactory = "kafkaListenerContainerFactory"
     )
     public void consume(PaymentCompletedAvroEvent avroEvent) {
-        handler.handle(mapper.toAppEvent(avroEvent));
+        var command = new PayOrderCommand(avroEvent.getOrderId());
+        payOrderHandler.handle(command);
     }
 }
